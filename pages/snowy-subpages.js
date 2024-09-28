f38826d72eb0189916488bceec72c3484fdfe7b0
Snowi <136918527+LynSnowie@users.noreply.github.com>
2024-09-28 07:50:26 +0100

    Add files via upload

--- pages/snowy-subpages.js
+++ pages/snowy-subpages.js
@@ -1,16 +1,17 @@
 // Define the menu items
-const menuItems = [
-    { name: "Home", url: "/" },
-    { name: "About Snowy", url: "/pages/about_snowy" },
-    // { name: "About Lolie", url: "file:///I:/new%20silly%20site/pages/about_lolie" }, // Fuck you Lynn/lolie. you crazy nigga trying to break my relationship and me.
-    { name: "About Glowy", url: "/pages/about_snowglow" },
-    // Add more items here as needed
+const snowmenuItems = [
+    { name: "Home", url: "/pages/about_snowy" },
+    { name: "Info", url: "/pages/about_snowy/info" },
+    { name: "For Fun", url: "/pages/about_snowy/for_fun.html" },
+    { name: "Snowy's Lore", url: "/pages/about_snowy/info/lore.html" },
+    { name: "Relationships", url: "/pages/about_snowy/relationships" },
+    { name: "Music", url: "/pages/about_snowy/music" },
 ];
 
 function generateNavItems(navId) {
     const navUl = document.getElementById(navId);
 
-    menuItems.forEach(item => {
+    snowmenuItems.forEach(item => {
         const li = document.createElement('li');
         const a = document.createElement('a');
         a.href = item.url;
@@ -20,23 +21,23 @@ function generateNavItems(navId) {
     });
 }
 
-generateNavItems('header-nav');
-generateNavItems('footer-nav');
+generateNavItems('subheader-nav');
+generateNavItems('subfooter-nav');
 
-const navLinks = document.querySelectorAll('header nav ul li a, footer nav ul li a');
+const snownavLinks = document.querySelectorAll('subheader nav ul li a, footer nav ul li a');
 
-navLinks.forEach(link => {
+snownavLinks.forEach(link => {
     let colorInterval;
 
     link.addEventListener('mouseenter', () => {
         let colorIndex = 0;
-        const colors = [
+        const snowcolors = [
             'rgba(0, 255, 149)',  // Mint
             'rgba(132, 0, 255)'  // Purple
         ];
 
         colorInterval = setInterval(() => {
-            link.style.boxShadow = `0 0 15px 5px ${colors[colorIndex]}, 0 0 15px 5px ${colors[1 - colorIndex]}`;
+            link.style.boxShadow = `0 0 15px 5px ${snowcolors[colorIndex]}, 0 0 15px 5px ${colors[1 - colorIndex]}`;
             colorIndex = 1 - colorIndex; // Toggle between 0 and 1
         }, 1000); // Change color every second
     });
